8e69648c5d4776c2f11741ff631eceeb4fcf4ba9
johku <johannes.hakkarainen@helsinki.fi>
2020-11-25 13:01:18 +0200

    UserInterfaceTest class

--- src/test/java/recommendation_library/UserInterfaceTest.java
+++ src/test/java/recommendation_library/UserInterfaceTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package recommendation_library.domain;
+package recommendation_library;
 
 /**
  *
