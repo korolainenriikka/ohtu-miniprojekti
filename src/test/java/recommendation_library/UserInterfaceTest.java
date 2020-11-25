3f10db9daa6b060ee34589ebeecd3be13e8300c7
johku <johannes.hakkarainen@helsinki.fi>
2020-11-25 13:09:17 +0200

    UserInterfaceTest alustus

--- src/test/java/recommendation_library/UserInterfaceTest.java
+++ src/test/java/recommendation_library/UserInterfaceTest.java
@@ -9,6 +9,23 @@ package recommendation_library;
  *
  * @author jhku
  */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class UserInterfaceTest {
+    
+    UserInterface UI;
+    
+    @Before
+    public void setUp(){
+        UI = mock(UserInterface.class);
+    }
     
 }
